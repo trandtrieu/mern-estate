0fce769f45136f362ec81b27b7f6bc43f64120b6
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2024-01-12 21:35:48 +0700

    update newest

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+        <span className="text-slate-500 ">Sahand </span>
+        <span className="text-slate-700">Estate </span>
+      </h1>
+    </header>
+  );
+}
